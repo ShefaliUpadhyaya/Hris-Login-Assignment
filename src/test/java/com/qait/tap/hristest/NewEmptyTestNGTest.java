57e8b8dbd7b06b22b90064c37c4c21e3b3182fb3
ShefaliUpadhyaya <40260444+ShefaliUpadhyaya@users.noreply.github.com>
2018-06-21 16:17:28 +0530

    Add files via upload

---
+++ src/test/java/com/qait/tap/hristest/NewEmptyTestNGTest.java
@@ -0,0 +1,51 @@
+package com.qait.tap.hristest;
+
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class NewEmptyTestNGTest {
+    
+    WebDriver driver;
+    
+    LoginForm loginForm;
+    @Test
+    public void attempt_Login_With_Incorrect_Password_Should_Render_Error_Message(){
+        Assert.assertTrue(loginForm
+                .loginWithIncorrectCredentials("INVALID_USERNAME", "INVALID_PASSWORD").contains("Invalid Login"));
+    }
+    
+    @Test(dependsOnMethods = {"attempt_Login_With_Incorrect_Password_Should_Render_Error_Message"})
+    public void attempt_Login_With_No_Password_Should_Annotate_Black_Password_Field(){
+        loginForm.login("raman", "");
+        // red border in password entry
+        Assert.assertTrue(loginForm.isPasswordEntryAnnotated());  
+    }
+    
+    @Test(expectedExceptions = UnhandledAlertException.class ,dependsOnMethods = {"attempt_Login_With_No_Password_Should_Annotate_Black_Password_Field"})
+    public void attempt_Login_With_Correct_Credentials_Should_Not_Render_PasswordField() {
+    	loginForm.loginWithCorrectCredentials("raman", "qainfotech");
+    	Assert.assertFalse(loginForm.loginWithCorrectCredentialsDoNotRenderLoginPage());
+    }
+    
+    @Test(expectedExceptions = UnhandledAlertException.class ,dependsOnMethods = {"attempt_Login_With_Correct_Credentials_Should_Not_Render_PasswordField"})
+    public void attempt_Login_With_Correct_Credentials_Should_Open_TimeSheet() {
+    	loginForm.loginWithCorrectCredentials("raman", "qainfotech");
+    	Assert.assertTrue(driver.getCurrentUrl().contains("employee"));
+    }
+    
+    @BeforeClass
+    public void launchBrowser(){
+        driver = new ChromeDriver();
+        driver.get("https://s-hris.qainfotech.com");
+        loginForm = new LoginForm(driver);
+    }
+    
+    @AfterClass
+    public void closeBrowser(){
+        driver.quit();
+    }
+    
+}
